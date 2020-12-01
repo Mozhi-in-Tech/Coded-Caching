f1343701e11feea8d46139ba431b5777b15ada39
Mozhi-in-Tech <58741950+Mozhi-in-Tech@users.noreply.github.com>
2020-12-01 11:36:34 +0100

    Add files via upload
    Parameters for simulation

---
+++ setting.py
@@ -0,0 +1,11 @@
+
+Iter1 = 50 # number iteration for generating random topology
+Iter2 = 20 # numebr of iteration for generating random cache configuration for each user
+mu =0.2  # normalized cache size at each user
+L_array =  [2,5,10,15]  # cache configuration values
+
+Rnw = 450 # a Disk with radius Rwn, this value determine average number of helper in the area
+KperCell= 20 # average number of users in each cell
+delta = 0.2 # a_interf/a_cell
+C_E= 1 # access capacity
+C_F= 30 # fronthaul capacity
